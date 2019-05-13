89c07ce0014d75f67b0007e6760785121c0b8aca
Barbara <princesita_1786@hotmail.com>
2019-05-13 19:02:24 -0300

    primero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MaterialModule } from './material.module';
     BrowserAnimationsModule,
     BrowserModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
