e81a3d414710ce9d1324c2c4d3734bc80f9d2ba8
Ezequiel Falcón <ezequiel.falcon@gpesistemas.com.ar>
2019-05-13 18:43:42 -0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
+
+})
+export class MaterialModule { }
