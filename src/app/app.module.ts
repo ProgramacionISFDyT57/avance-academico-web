5e57cd2404f4111ffd1686db9b5113037865c008
Fernando Cortese <cortesefernando@gmail.com>
2019-05-14 17:46:38 -0300

    modulo angular material movido a carpeta modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './modulos/material.module';
 import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
