7ed8bfa2fec33c4dd4860f25b208e7db3b1c5e3b
Barbara <princesita_1786@hotmail.com>
2019-05-27 18:31:34 -0300

    login http

--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  backend= 'https://avance-academico-backend.herokuapp.com'
+  constructor(
+    private http:HttpClient
+  ) { };
+
   public ini_sesion(email: string, clave:string){
-    console.log(email, clave)
+    const body={
+      mail: email,
+      clave: clave,
+    }
+    return this.http.post(this.backend +'/login',body)
   }
 
-  constructor() { }
+
 }
